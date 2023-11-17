c42ed13091931aae5b2b9391c6e9ff4241582d5c
sifisKoen <sifis.k.i@gmail.com>
2023-11-17 23:04:05 +0100

    Update auto install nerdfont script

--- scripts/nerdfont_install.sh
+++ scripts/nerdfont_install.sh
@@ -1,31 +1,76 @@
 #!/bin/sh
 
-if [ "$(id -u)" -ne 0 ]; then
-   echo "Please run this script with sudo."
-   exit 1
-fi
+check_root() {
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Please run this script with sudo."
+        exit 1
+    fi
+}
 
-cd /tmp || exit
+display_fonts() {
+    echo "Available Nerd Fonts:"
+    set -f  # Disable globbing for safe word splitting
+    i=1
+    for font in $fonts; do
+        echo "$i. $font"
+        i=$((i + 1))
+    done
+    set +f  # Re-enable globbing
+}
 
-# List the nerd fonts as a space-separated string
-fonts="CascadiaCode FiraCode Go-Mono Hack Inconsolata Iosevka JetBrainsMono Mononoki RobotoMono SourceCodePro UbuntuMono"
+read_user_selection() {
+    echo "Enter the numbers of the fonts you want to install, separated by spaces (e.g., 1 3 5):"
+    read -r user_input
+}
 
-# Iterate over the font names
-for font in $fonts; do
-    # Download the font zip file
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/$font.zip
-    
-    # Create a directory for the font within /usr/share/fonts/
-    mkdir -p /usr/share/fonts/$font
-    
-    # Unzip the font directly into the directory we just created
-    unzip $font.zip -d /usr/share/fonts/$font/
-    
-    # Remove the downloaded zip file
-    rm $font.zip
-done
+process_selection() {
+    selected_fonts=""
+    for num in $user_input; do
+        i=1
+        for font in $fonts; do
+            if [ "$i" -eq "$num" ]; then
+                selected_fonts="$selected_fonts $font"
+                break
+            fi
+            i=$((i + 1))
+        done
+    done
+}
 
-# Update font cache
-fc-cache -fv
+install_fonts() {
+    for font in $selected_fonts; do
+        echo "Installing $font..."
 
-echo "Nerd Fonts installed successfully!"
+        # Download the font zip file
+        wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/$font.zip
+        
+        # Create a directory for the font
+        mkdir -p /usr/share/fonts/$font
+        
+        # Unzip the font
+        unzip $font.zip -d /usr/share/fonts/$font/
+        
+        # Remove the downloaded zip file
+        rm $font.zip
+    done
+
+    # Update font cache
+    fc-cache -fv
+}
+
+main() {
+    check_root
+    cd /tmp || exit
+
+    # List of Nerd Fonts
+    fonts="CascadiaCode FiraCode Go-Mono Hack Inconsolata Iosevka JetBrainsMono Mononoki RobotoMono SourceCodePro UbuntuMono"
+
+    display_fonts
+    read_user_selection
+    process_selection
+    install_fonts
+
+    echo "Selected Nerd Fonts installed successfully!"
+}
+
+main
